11b00a99857e71aad57cb7e428b02973bc8208d9
Finger Chung <260910416@qq.com>
2019-09-24 19:30:08 +0800

    从相册读取图片

---
+++ FCLive/FCImageFilterView.m
@@ -0,0 +1,27 @@
+//
+//  FCImageFilterView.m
+//  FCLive
+//
+//  Created by Zhijia Zhong on 2019/9/24.
+//  Copyright © 2019 Zhijia Zhong. All rights reserved.
+//
+
+#import "FCImageFilterView.h"
+
+@interface FCImageFilterView ()
+@property (weak, nonatomic) IBOutlet UIImageView *originalImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *resultImageView;
+
+@end
+
+@implementation FCImageFilterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- FCLive/FCImageViewController.m
+++ FCLive/FCImageViewController.m
@@ -8,11 +8,12 @@
 
 #import "FCImageViewController.h"
 #import <GPUImage/GPUImage.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import "FCImageFilterView.h"
 
-@interface FCImageViewController ()
+@interface FCImageViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+@property (weak, nonatomic) IBOutlet UIScrollView *contentView;
 
 @end
 
@@ -21,8 +22,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    UIImage *image = [UIImage imageNamed:@"meinv.jpg"];
-    self.imageView.image = image;
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Album" style:UIBarButtonItemStylePlain target:self action:@selector(didClickRightItem:)];
+//    
+//    UIImage *image = [UIImage imageNamed:@"meinv.jpg"];
+//    self.imageView.image = image;
     
 //    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:image];
 //    GPUImageSepiaFilter *stillImageFilter = [[GPUImageSepiaFilter alloc] init];
@@ -34,19 +38,42 @@
 //    self.imageView2.image = resultImage;
     
     // 只使用一个滤镜时可以用快捷方法
-    GPUImageSepiaFilter *stillImageFilter2 = [[GPUImageSepiaFilter alloc] init];
+//    GPUImageSepiaFilter *stillImageFilter2 = [[GPUImageSepiaFilter alloc] init];
+//    UIImage *resultImage = [stillImageFilter2 imageByFilteringImage:image];
+//    self.imageView2.image = resultImage;
+}
+
+- (void)didClickRightItem:(UIBarButtonItem *)item {
+    UIImagePickerController *vc = [[UIImagePickerController alloc] init];
+    vc.delegate = self;
+    vc.mediaTypes = @[(NSString *) kUTTypeImage];
+    vc.allowsEditing = NO;
+    [self.navigationController presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    [self changeImage:image];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)configFilterViews {
+    FCImageFilterView *view = [[NSBundle mainBundle] loadNibNamed:@"FCImageFilterView" owner:self options:nil].firstObject;
+    [self.contentView addSubview:view];
+}
+
+- (void)changeImage:(UIImage *)image {
+    GPUImage3x3ConvolutionFilter *stillImageFilter2 = [[GPUImage3x3ConvolutionFilter alloc] init];
+    stillImageFilter2.convolutionKernel = (GPUMatrix3x3){
+        {-1.f, -1.f, -1.f},
+        {-1.f, 8.f, -1.f},
+        {-1.f, -1.f, -1.f}
+    };
     UIImage *resultImage = [stillImageFilter2 imageByFilteringImage:image];
-    self.imageView2.image = resultImage;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
